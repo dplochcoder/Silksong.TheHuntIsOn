d79fdefb1bf8207792ae919e527aa6ff00742f6a
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-01-24 18:40:39 -0500

    Split into Bind and Stats modules.

---
+++ Modules/BindModule.cs
@@ -0,0 +1,87 @@
+﻿using HutongGames.PlayMaker;
+using Silksong.FsmUtil;
+using Silksong.ModMenu.Elements;
+using Silksong.ModMenu.Models;
+using Silksong.TheHuntIsOn.Menu;
+using Silksong.TheHuntIsOn.Modules.Lib;
+using Silksong.TheHuntIsOn.SsmpAddon.PacketUtil;
+using Silksong.TheHuntIsOn.Util;
+using SSMP.Networking.Packet;
+using System.Collections.Generic;
+
+namespace Silksong.TheHuntIsOn.Modules;
+
+internal class BindSettings : NetworkedCloneable<BindSettings>
+{
+    public int HealMasks = 3;
+    public int MultibinderHealMasks = 2;
+    public int SilkCost = 9;
+    public float TimePenalty = 1f;
+
+    public override void ReadData(IPacket packet)
+    {
+        HealMasks.ReadData(packet);
+        MultibinderHealMasks.ReadData(packet);
+        SilkCost.ReadData(packet);
+        TimePenalty.ReadData(packet);
+    }
+
+    public override void WriteData(IPacket packet)
+    {
+        HealMasks.WriteData(packet);
+        MultibinderHealMasks.WriteData(packet);
+        SilkCost.WriteData(packet);
+        TimePenalty.WriteData(packet);
+    }
+}
+
+internal class BindModule : GlobalSettingsModule<BindModule, BindSettings, BindSubMenu>
+{
+    protected override BindModule Self() => this;
+
+    public override string Name => "Bind";
+
+    public override ModuleActivationType ModuleActivationType => ModuleActivationType.AnyConfiguration;
+
+    private static void SetSilkCost(FsmInt fsmInt)
+    {
+        if (Instance != null) fsmInt.Value = Instance.GlobalConfig.SilkCost;
+    }
+
+    private static void EditBindFsm(PlayMakerFSM fsm)
+    {
+        fsm.GetState("Can Bind?")!.InsertAction(0, IfEnabled(s => fsm.FsmVariables.GetFsmInt("Silk Cost").Value = s.SilkCost));
+        fsm.GetState("Set Normal")!.InsertAction(3, IfEnabled(s => fsm.FsmVariables.GetFsmInt("Heal Amount").Value = s.HealMasks));
+        fsm.GetState("Multi Bind")!.AddAction(IfEnabled(s => fsm.FsmVariables.GetFsmInt("Heal Amount").Value = s.MultibinderHealMasks));
+        fsm.GetState("Bind Shared")!.InsertAction(0, IfEnabled(s => fsm.FsmVariables.GetFsmFloat("Bind Time").Value *= s.TimePenalty));
+    }
+
+    // FIXME: Fix UI
+    static BindModule() => Events.AddFsmEdit("Hero_Hornet", "Bind", EditBindFsm);
+}
+
+internal class BindSubMenu : ModuleSubMenu<BindSettings>
+{
+    private readonly ChoiceElement<int> HealMasks = new("Heal Masks", CollectionUtil.IntRangeModel(0, 10), "Number of masks to heal when binding.");
+    private readonly ChoiceElement<int> MultibinderHealMasks = new("Multibinder Heal Masks", CollectionUtil.IntRangeModel(0, 5), "Number of masks to heal when multi-binding.");
+    private readonly ChoiceElement<int> SilkCost = new("Silk Cost", CollectionUtil.IntRangeModel(1, 18), "Number of silk spools required to bind.");
+    private readonly ChoiceElement<float> TimePenalty = new("Time Penalty", ChoiceModels.ForValues([0.25f, 0.5f, 0.75f, 1f, 1.5f, 2f, 3f]), "Multiplier on the time it takes to bind.");
+
+    public override IEnumerable<MenuElement> Elements() => [HealMasks, MultibinderHealMasks, SilkCost, TimePenalty];
+
+    internal override void Apply(BindSettings data)
+    {
+        HealMasks.Value = data.HealMasks;
+        MultibinderHealMasks.Value = data.MultibinderHealMasks;
+        SilkCost.Value = data.SilkCost;
+        TimePenalty.Value = data.TimePenalty;
+    }
+
+    internal override BindSettings Export() => new()
+    {
+        HealMasks = HealMasks.Value,
+        MultibinderHealMasks = MultibinderHealMasks.Value,
+        SilkCost = SilkCost.Value,
+        TimePenalty = TimePenalty.Value,
+    };
+}
---
+++ Modules/StatsModule.cs
@@ -0,0 +1,73 @@
+﻿using Silksong.ModMenu.Elements;
+using Silksong.TheHuntIsOn.Menu;
+using Silksong.TheHuntIsOn.Modules.Lib;
+using Silksong.TheHuntIsOn.SsmpAddon.PacketUtil;
+using Silksong.TheHuntIsOn.Util;
+using SSMP.Networking.Packet;
+using System.Collections.Generic;
+
+namespace Silksong.TheHuntIsOn.Modules;
+
+internal class StatsSettings : NetworkedCloneable<StatsSettings>
+{
+    public int StartingMasks = 5;
+    public int StartingSilkSpools = 9;
+
+    public override void ReadData(IPacket packet)
+    {
+        StartingMasks.ReadData(packet);
+        StartingSilkSpools.ReadData(packet);
+    }
+
+    public override void WriteData(IPacket packet)
+    {
+        StartingMasks.WriteData(packet);
+        StartingSilkSpools.WriteData(packet);
+    }
+}
+
+internal class StatsModule : GlobalSettingsModule<StatsModule, StatsSettings, StatsSubMenu>
+{
+    protected override StatsModule Self() => this;
+
+    public override string Name => "Stats";
+
+    public override ModuleActivationType ModuleActivationType => ModuleActivationType.AnyConfiguration;
+
+    public override void OnEnabled() => PrepatcherPlugin.PlayerDataVariableEvents<int>.OnGetVariable += ModifyCoreStats;
+
+    public override void OnDisabled() => PrepatcherPlugin.PlayerDataVariableEvents<int>.OnGetVariable += ModifyCoreStats;
+
+    private int ModifyCoreStats(PlayerData playerData, string name, int orig)
+    {
+        if (Instance == null) return orig;
+
+        var config = Instance.GlobalConfig;
+        return name switch
+        {
+            nameof(PlayerData.maxHealth) => orig + (config.StartingMasks - 5),
+            nameof(PlayerData.silkMax) => orig + (config.StartingSilkSpools - 9),
+            _ => orig
+        };
+    }
+}
+
+internal class StatsSubMenu : ModuleSubMenu<StatsSettings>
+{
+    private readonly ChoiceElement<int> StartingMasks = new("Starting Masks", CollectionUtil.IntRangeModel(1, 10), "Number of masks players start with.");
+    private readonly ChoiceElement<int> StartingSilkSpools = new("Starting Silk Spools", CollectionUtil.IntRangeModel(1, 18), "Number of silk spools players start with.");
+
+    public override IEnumerable<MenuElement> Elements() => [StartingMasks, StartingSilkSpools];
+
+    internal override void Apply(StatsSettings data)
+    {
+        StartingMasks.Value = data.StartingMasks;
+        StartingSilkSpools.Value = data.StartingSilkSpools;
+    }
+
+    internal override StatsSettings Export() => new()
+    {
+        StartingMasks = StartingMasks.Value,
+        StartingSilkSpools = StartingSilkSpools.Value,
+    };
+}
